3852969b1a96fda085de9a80a0d728a44b249085
Unknown <vilhelmen@gmail.com>
2018-07-29 21:16:40 -0500

    Expose FailThreadPool

--- FailPool/__init__.py
+++ FailPool/__init__.py
@@ -7,6 +7,6 @@ assert sys.version_info.major == 3 and sys.version_info.minor in (
     6, 7) and platform.python_implementation() == 'CPython'
 
 from .ThreadPoolExecutor import FailThreadPoolExecutor
-from .multiprocessing import FailPool
+from .multiprocessing import FailPool, FailThreadPool
 
-__all__ = ['FailThreadPoolExecutor', 'FailPool']
+__all__ = ['FailThreadPoolExecutor', 'FailPool', 'FailThreadPool']
